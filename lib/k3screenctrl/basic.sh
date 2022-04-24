a370156e2ab011765770f87dd3d00f414a6bde9f
rya <103394626+YLEDE@users.noreply.github.com>
2022-04-24 13:10:32 +0800

    Update basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -16,18 +16,12 @@ FW_VERSION=${DISTRIB_REVISION:0:17}
 
 echo $PRODUCT_NAME
 
-if [ $(uci get k3screenctrl.@general[0].showmore) -eq 1 ]; then
-    echo U:$CPU_TEMP *C
-    used=`free | grep Mem | awk '{print$3}'`
-    all=`free | grep Mem | awk '{print$2}'`
-    LOAD=`uptime | awk -F "average:" '{print$2}' | awk -F "," '{print$1}'`
-    UPTIME=`uptime | awk -F "," '{print$1}'|awk '{print"up " $3" " $4}'`
-    echo U:$LOAD R:$((100*$used/$all))%
-    echo $UPTIME
-    echo $DISTRIB_DESCRIPTION
+if [ $(uci get k3screenctrl.@general[0].cputemp) -eq 1 ]; then
+    echo $HW_VERSION $CPU_TEMP
 else
     echo $HW_VERSION
-    echo $FW_VERSION
-    echo $FW_VERSION
-    echo $MAC_ADDR
 fi
+
+echo $FW_VERSION
+echo $FW_VERSION
+echo $MAC_ADDR
