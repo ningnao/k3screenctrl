9d590ba54d73f7ca0e2200087a6d1fc188d30e76
ningnao <ningnao@foxmail.com>
2022-12-11 12:53:55 +0800

    port.sh:修改检测方式
    OpenWrt 21.02 版本之后使用 iproute2包 替代 swconfig 配置 vlan

--- lib/k3screenctrl/port.sh
+++ lib/k3screenctrl/port.sh
@@ -4,7 +4,8 @@ print_eth_port_status() {
     local port=$1
 
     # One `swconfig dev switch0 show` wastes more time than 4 `port show`
-    if [ -n "`swconfig dev switch0 port $port show | grep \"link:up\"`" ]; then
+    # OpenWrt 21.02 版本之后使用 DSA 替代 swconfig
+    if [ -n "`ifconfig $port | grep \"RUNNING\"`" ]; then
         echo 1
     else
         echo 0
@@ -19,8 +20,8 @@ print_usb_port_status() {
     fi
 }
 
-print_eth_port_status 1 # Port 1 is LAN1 on label
-print_eth_port_status 0 # Port 0 is LAN2 on label
-print_eth_port_status 2 # LAN3
-print_eth_port_status 3 # WAN
+print_eth_port_status lan1 # LAN1
+print_eth_port_status lan2 # LAN2
+print_eth_port_status lan3 # LAN3
+print_eth_port_status wan # WAN
 print_usb_port_status
